125b1574a81e2e37c576fba2d0b06f507e17fd7e
Michael <michaelcthomas04@gmail.com>
2025-08-29 20:32:22 -0700

    chore: satisfy lint rule for unused error handler args

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(healthRouter);
 app.use('/tasks', tasksRouter);
 
 // Global error handler: never crash; return JSON
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
     console.error('Unhandled error:', err && err.stack ? err.stack : err);
     res.status(500).json({ error: 'internal_error' });
 });
